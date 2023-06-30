d7b8f4b8dbd329b48a69c348d6b48e3d1b2b9348
Harsh Kargeti <harshkargeti123@gmail.com>
2023-06-30 18:19:47 +0530

    ADD Login and Register page

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -1,14 +1,78 @@
 import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
-// import {toast} from "read-toastify";
+import {toast} from "react-toastify";
+import customFetch from "../utils/axios";
+import { addUserToLocalStorage, getUserFromLocalStorage } from "../utils/localStorage";
 
 const initialState = {
     isLoading: false,
-    user: null,
+    user: getUserFromLocalStorage(),
 };
+
+export const registerUser = createAsyncThunk(
+    'user/registerUser', 
+    async (user, thunkAPI) => {
+        try{
+            const resp = await customFetch.post('/auth/register', user);
+            console.log(resp.data)
+            return resp.data
+          
+        } catch(error) {
+            return thunkAPI.rejectWithValue(error.response.data.msg);
+        }
+})
+
+export const loginUser = createAsyncThunk(
+    'user/loginUser', 
+    async (user, thunkAPI) => {
+        try{
+            const resp = await customFetch.post('/auth/login', user);
+            console.log(resp.data)
+            return resp.data
+          
+        } catch(error) {
+            return thunkAPI.rejectWithValue(error.response.data.msg);
+        }
+})
 
 const userSlice = createSlice({
     name: 'user',
     initialState,
+    extraReducers: {
+        [registerUser.pending]: (state) => {
+            state.isLoading = true;
+        },
+        [registerUser.fulfilled]: (state, {payload}) => {
+            const { user } = payload;
+            console.log(payload)
+            state.isLoading = false;
+            state.user = user;
+            addUserToLocalStorage(user)
+            toast.success(`Hello there ${user.name}`)
+        },
+        [registerUser.rejected]: (state, {payload}) => {
+            state.isLoading = false;
+            toast.error(payload);
+        },
+        [loginUser.pending]: (state) => {
+            state.isLoading = true;
+        },
+
+        [loginUser.fulfilled]: (state, {payload}) => {
+            const { user } = payload;
+            console.log(payload)
+            state.isLoading = false;
+            state.user = user;
+            addUserToLocalStorage(user)
+
+            toast.success(`Welcome back ${user.name}`)
+        },
+        [loginUser.rejected]: (state, {payload}) => {
+            state.isLoading = false;
+            toast.error(payload);
+        },
+        
+    }
 });
-console.log(userSlice);
+
+// console.log(userSlice);
 export default userSlice.reducer
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {Logo, FormRow} from '../components';
 import Wrapper from '../assets/wrappers/RegisterPage';
 import { toast } from 'react-toastify';
+import { useDispatch, useSelector } from "react-redux";
+import { registerUser } from "../features/userSlice";
+import { loginUser } from "../features/userSlice";
+import { useNavigate } from "react-router-dom";
+
 const initialState = {
     name: '',
     email: '',
@@ -12,27 +17,47 @@ const initialState = {
 function Register() {
     const [values, setValues] = useState(initialState);
 
+    const {user, IsLoading} = useSelector((store) => store.user);
+    const dispatch = useDispatch();
+
+    const navigate = useNavigate();
+
     const handleChange = (e) => {
         const name = e.target.name
         const value = e.target.value
-        console.log(`${name}:${value}`);
+        // console.log(`${name}:${value}`);
 
         setValues({ ...values, [name]: value});
     }
 
     const onSubmit = (e) => {
         e.preventDefault();
-        console.log(e.target);
+      
         const {name, email, password, isMember} = values;
         if(!email || !password || (!isMember && !name)){
             toast.error('Please fill out all fields');
+            return;
         }
+
+        if(isMember){
+            dispatch(loginUser({ email: email, password: password}));
+            return;
+        }
+        dispatch(registerUser({name, email, password}));
     }
 
     const toggleMember = () => {
         setValues({ ...values, isMember: !values.isMember});
-        console.log(values.isMember);
-    }
+       
+    };
+
+    useEffect(() => {
+        if(user) {
+            setTimeout(() => {
+                navigate('/');
+            }, 2000);
+        }
+    },[user]);
 
     return(
         <Wrapper className="full-page">
@@ -61,14 +86,14 @@ function Register() {
                     handleChange={handleChange}
                 />
 
-                <button type="submit" className="btn btn-block">
-                    submit
+                <button type="submit" className="btn btn-block" disabled={IsLoading}>
+                    {IsLoading ? 'loading...' : 'submit'}
                 </button>
 
                 <p>
 
-                    <button type="button" onClick={toggleMember}>
-                      wfd
+                    <button type="button" onClick={toggleMember} className="member-btn">
+                      {values.isMember ? 'Register' : 'Login'}
                     </button>
                 </p>
             </form>
